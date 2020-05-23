3cb1f56a4a2ce25e8aa7a87dc42423132700c953
Victor ROULLEAU <vno4@laposte.net>
2020-05-23 11:41:51 +0200

    Récupération de l'emplacement terminal + génération du bonus principal.

--- src/app/generator/item-generator.service.ts
+++ src/app/generator/item-generator.service.ts
@@ -5,6 +5,7 @@ import { ItemDescriptor } from '../data/ItemDescriptor';
 import { Subject, Observable, from } from 'rxjs';
 import { ItemSlot } from '../data/itemSlot';
 import { sample } from 'lodash';
+import { MainBonusRef } from '../data/referenceTables/mainBonusRef';
 
 @Injectable({
   providedIn: 'root'
@@ -21,9 +22,9 @@ export class ItemGeneratorService {
       let descriptor: ItemDescriptor = new ItemDescriptor();
       descriptor.level = itemDescriptor.level ?? this.generateRandomLevel();
       descriptor.rarityIndex = itemDescriptor.rarityIndex ?? this.generateRandomRarityIndex();
-      descriptor.slot = Object.assign({}, itemDescriptor.slot ?? this.generateRandomSlot());
+      descriptor.slot = this.getTerminalSlot(Object.assign({}, itemDescriptor.slot ?? this.generateRandomSlot()));
 
-      items.push(new Item(descriptor, "fluff", "main bonus", []));
+      items.push(new Item(descriptor, "fluff", this.selectMainBonus(descriptor), []));
     }
 
     this.generatedItemsSource.next(items);
@@ -42,7 +43,47 @@ export class ItemGeneratorService {
     return sample(this.referenceDataService.rarityTable).rarityIndex;    
   }
 
+  private getTerminalSlot(originSlot:ItemSlot): ItemSlot {
+    if (originSlot.terminal) return originSlot;
+    return this.getTerminalSlot(this.referenceDataService.slotsTable.find(s => s.parentSlotKey == originSlot.key));
+  }
+
+  private isCurrentOrParentSlot(originSlot:ItemSlot, searchedKey:string): boolean {
+    if(originSlot.key == searchedKey) return true;
+    else if(!originSlot.parentSlotKey) return false;
+    return this.isCurrentOrParentSlot(this.referenceDataService.slotsTable.find(s => s.key == originSlot.parentSlotKey), searchedKey);
+  }
+
+  private selectMainBonus(descriptor:ItemDescriptor): string {
+    let bonus: MainBonusRef[] = this.referenceDataService.mainBonusRef.filter(b => 
+      descriptor.level >= b.minLevel 
+      && descriptor.rarityIndex >= b.minRarityIndex 
+      && this.isCurrentOrParentSlot(descriptor.slot, b.slotKey));
+      
+    if(bonus.length > 0){
+      let sum: number = 0;
+      let weightedBonus: WeightedBonus[] = [];
+      for(let i:number = 0; i < bonus.length; i++){
+        let curr = bonus[i];
+        weightedBonus.push(new WeightedBonus(curr.weight + sum, curr));
+        sum += curr.weight;
+      }
+      let rnd = Math.random() * sum;
+      for(let i:number =0; i < weightedBonus.length; i++){
+        let curr = weightedBonus[i];
+        if(rnd <= curr.weight) return curr.bonus.bonus;
+      }
+    }
+    return null;
+  }
+
+
   private generatedItemsSource: Subject<Item[]> = new Subject();
   public generatedItems$: Observable<Item[]> = this.generatedItemsSource.asObservable();
 
+}
+
+class  WeightedBonus {
+  constructor(public weight:number, public bonus:MainBonusRef) {    
+  }
 }
