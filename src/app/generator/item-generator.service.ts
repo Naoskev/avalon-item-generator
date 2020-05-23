6a3feb06a103a08b386ef568397a6d5420643373
Victor ROULLEAU <vno4@laposte.net>
2020-05-23 10:51:22 +0200

    Ajout de la génération aléatoire + correction des valeurs null dans le formulaire.

--- src/app/generator/item-generator.service.ts
+++ src/app/generator/item-generator.service.ts
@@ -2,8 +2,9 @@ import { Injectable } from '@angular/core';
 import { Item } from '../data/item';
 import { ReferenceDataService } from './reference-data.service';
 import { ItemDescriptor } from '../data/ItemDescriptor';
-import { Subject, Observable } from 'rxjs';
-
+import { Subject, Observable, from } from 'rxjs';
+import { ItemSlot } from '../data/itemSlot';
+import { sample } from 'lodash';
 
 @Injectable({
   providedIn: 'root'
@@ -14,13 +15,31 @@ export class ItemGeneratorService {
 
   public generate(itemDescriptor:ItemDescriptor, nbToGenerate: number = 10)
   {
-
+    console.log(itemDescriptor);
     let items: Item[] = [];
     for(let i:number = 0; i < nbToGenerate; i++){
-      items.push(new Item(itemDescriptor, "fluff", "main bonus", []));
+      let descriptor: ItemDescriptor = new ItemDescriptor();
+      descriptor.level = itemDescriptor.level ?? this.generateRandomLevel();
+      descriptor.rarityIndex = itemDescriptor.rarityIndex ?? this.generateRandomRarityIndex();
+      descriptor.slot = Object.assign({}, itemDescriptor.slot ?? this.generateRandomSlot());
+
+      items.push(new Item(descriptor, "fluff", "main bonus", []));
     }
 
     this.generatedItemsSource.next(items);
+  }
+
+
+  private generateRandomLevel(): number {
+    return Math.floor(1 + Math.random() *10);
+  }
+
+  private generateRandomSlot(): ItemSlot {
+    return sample(this.referenceDataService.slotsTable);
+  }
+
+  private generateRandomRarityIndex(): number {
+    return sample(this.referenceDataService.rarityTable).rarityIndex;    
   }
 
   private generatedItemsSource: Subject<Item[]> = new Subject();
