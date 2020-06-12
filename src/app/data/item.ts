09e9af03edf007b374f2d9279d3c98ba0e45f150
Naoskev <vno4@laposte.net>
2020-06-12 23:31:22 +0200

    mise en forme globale

--- src/app/data/item.ts
+++ src/app/data/item.ts
@@ -25,19 +25,19 @@ export class Item {
   
     public getHP(): string{
       let sum = sumBy(this.secondaryBonus.filter(s => s.kind === BonusKind.HP), i => i.bonusValue);
-      if(sum > 0) return "+ "+sum+ " PV";
+      if(sum > 0) return "+"+sum+ " PV";
       return "";
     }
   
     public getLook(): string {
       let look = this.secondaryBonus.find(i => i.kind === BonusKind.Look);
-      return look != null ? "+ "+look.bonusValue + " " + look.nature : "";
+      return look != null ? "+"+look.bonusValue + " " + look.nature : "";
     }
 
     private computeElements(kind: BonusKind){        
         let masteries = this.secondaryBonus.filter(m => m.kind == kind);
         
       let tt = groupBy(masteries, 'nature');
-      return sortBy(keys(tt)).map(s => "+ "+sumBy(tt[s], bn => bn.bonusValue)+ " " + s).join();
+      return sortBy(keys(tt)).map(s => "+"+sumBy(tt[s], bn => bn.bonusValue)+ " " + s).join();
     }
 }
