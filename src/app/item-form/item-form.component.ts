1ccb2e1cc5b9c0d9e833d75e20d7c4a3aa4bef0c
Victor ROULLEAU <vno4@laposte.net>
2020-08-30 18:24:44 +0200

    Ajout d'un controle sur la liste des raretés en fonction du niveau sélectionné.

--- src/app/item-form/item-form.component.ts
+++ src/app/item-form/item-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ReferenceDataService } from '../generator/reference-data.service';
 import { ItemDescriptor } from '../data/ItemDescriptor';
 import { ItemGeneratorService } from '../generator/item-generator.service';
+import { RarityRef } from '../data/referenceTables/rarityRef';
 
 @Component({
   selector: 'app-item-form',
@@ -14,6 +15,7 @@ export class ItemFormComponent implements OnInit {
    }
 
   ngOnInit(): void {
+    this.filterRarity();
   }
 
   onSubmit() { 
@@ -21,7 +23,27 @@ export class ItemFormComponent implements OnInit {
     console.log("Submit !");
   }
 
+  public filterRarity():void {
+    console.log("select level")
+    console.log(this.itemWantedDescriptor.level);
+    this.rarityList = this.referenceDataService.rarityTable.filter(r => this.itemWantedDescriptor.level == null || r.rarityMinLevel <= this.itemWantedDescriptor.level);
+    console.log(this.rarityList);
+    if(!this.rarityList.some(r => r.rarityIndex === this.itemWantedDescriptor.rarityIndex)){
+      this.itemWantedDescriptor.rarityIndex = null;      
+    }
+    console.log(this.itemWantedDescriptor.rarityIndex);
+  }
+
+  public selectRarity(): void {
+    console.log("select rarity")
+    console.log(this.itemWantedDescriptor.level);
+    console.log(this.itemWantedDescriptor.rarityIndex);
+
+  }
+
   public levelList = [...Array(11).keys()].splice(1);
+
+  public rarityList: RarityRef[];
 
   public displayedSlots = this.referenceDataService.slotsTable.filter(is => is.display);
 
